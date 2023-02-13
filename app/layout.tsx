56eae9ba12c196f7ffe0c147f72ca672d15edd4a
Blosuhm <zakhar.kruptsala@gmail.com>
2023-02-13 22:34:19 +0000

    some minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,10 @@ export default function RootLayout({
       <head />
       <body>
         <nav></nav>
-        <main className="container mx-auto px-4">{children}</main>
-        <footer></footer>
+        <main className="container mx-auto px-4 pb-96">{children}</main>
+        <footer className=" bg-black absolute w-full bottom-0 h-96">
+          <div className="container"></div>
+        </footer>
       </body>
     </html>
   );
--- app/students/[nmec]/page.tsx
+++ app/students/[nmec]/page.tsx
@@ -1,24 +1,30 @@
 import { students } from "./grades.json";
 import { StudentSchema } from "@/app/schema";
-import { record } from "zod";
+import z from "zod";
 
-const FullStudentSchema = record(StudentSchema);
-
+const FullStudentSchema = z.record(StudentSchema);
 const studentsObject = FullStudentSchema.parse(students);
 
-type Params = {
-  nmec: string;
-};
+const Props = z.object({
+  params: z.object({
+    nmec: z.string(),
+  }),
+});
 
-interface Props {
-  params: Params;
-}
+type Props = z.infer<typeof Props>;
 
 export default function NmecPage({ params }: Props) {
+  if (!studentsObject.hasOwnProperty(params.nmec)) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <h1 className="text-center">Student not found!</h1>
+      </div>
+    );
+  }
   return (
     <div className="h-screen flex items-center justify-center">
       <h1 className="text-center">
-        Best Average: {studentsObject[params.nmec].name}
+        Student Name: {studentsObject[params.nmec].name}
       </h1>
     </div>
   );
