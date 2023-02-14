f86874ca56c7a58164cef3939e5237c6de77bc70
Blosuhm <zakhar.kruptsala@gmail.com>
2023-02-14 22:18:56 +0000

    students page now has an accordion with every student

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -1,7 +1,46 @@
+"use client";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { students, subjects } from "@/grades-data/data/grades.json";
+import GradesSchema from "@/app/schema";
+
 export default function StudentPage() {
+  const studentsObject = GradesSchema.students.parse(students);
+  const subjectsObject = GradesSchema.subjects.parse(subjects);
+
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-center">Best Average: Bananas</h1>
+    <div>
+      {Object.entries(studentsObject).map(([nmec, student]) => {
+        return (
+          <Accordion key={nmec}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>
+                {nmec}: {student.name}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {Object.entries(student.subjects).map(([subject, grade]) => {
+                return (
+                  <div key={subject}>
+                    <Typography>
+                      {subjectsObject[subject].name}: {grade}
+                    </Typography>
+                  </div>
+                );
+              })}
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
     </div>
   );
 }
